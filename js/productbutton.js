56de3e2c17ba05257103a706f57da50a57f7156b
Georgiy Sichinava <danzels5000@gmail.com>
2023-10-31 13:33:21 +0300

    new responsive

---
+++ js/productbutton.js
@@ -0,0 +1,13 @@
+const buttonLeft = document.getElementById('button-left')
+const buttonRight = document.getElementById('button-right')
+const itemSteps = document.querySelector('.production__item-steps')
+
+
+buttonLeft.addEventListener('click', () => {
+    console.log('click')
+    itemSteps.classList.toggle('process__active')
+})
+
+buttonRight.addEventListener('click', () => {
+    itemSteps.classList.toggle('process__active')
+})
--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,17 @@
-// Функция для обработки видимости секции
-function handleIntersection(entries, observer) {
-  entries.forEach(entry => {
-      if (entry.isIntersecting) {
-          entry.target.style.opacity = '1';
-          entry.target.style.transform = 'translateY(0)';
-          observer.unobserve(entry.target); // Прекращаем отслеживание после анимации
-      }
-  });
+const sections = document.querySelectorAll('.section');
+
+let currentSection = 0;
+
+function scrollToSection(index) {
+    sections[index].scrollIntoView({ behavior: 'smooth' });
 }
 
-// Настройка IntersectionObserver
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.2 // Когда секция видна на 20%, начинаем анимацию
-};
-
-const observer = new IntersectionObserver(handleIntersection, options);
-
-// Находим секцию для анимации
-const animationSection = document.querySelectorAll('.animation-section');
-
-// Начинаем отслеживание
-observer.observe(animationSection);
+window.addEventListener('wheel', (event) => {
+    if (event.deltaY > 0 && currentSection < sections.length - 1) {
+        currentSection++;
+        scrollToSection(currentSection);
+    } else if (event.deltaY < 0 && currentSection > 0) {
+        currentSection--;
+        scrollToSection(currentSection);
+    }
+});
