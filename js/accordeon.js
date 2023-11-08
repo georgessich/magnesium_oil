56a9a0b36884582fae98c9731681157b9285bcac
Georgiy Sichinava <danzels5000@gmail.com>
2023-11-08 20:11:44 +0300

    fix trackpad

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,3 +1,169 @@
+// const accordeonItems = document.querySelectorAll(".accordeon__item");
+// const image = document.querySelector(".hair-product--img");
+
+// let isMobile = window.innerWidth < 725;
+// let originalImageHeight = image.getBoundingClientRect().height;
+
+// function toggleAccordionItem(item) {
+//   const content = item.querySelector(".accordeon__content");
+//   const isActive = item.classList.contains("active");
+
+//   accordeonItems.forEach((otherItem) => {
+//     if (otherItem !== item) {
+//       otherItem.classList.remove("active");
+//       otherItem.querySelector(".accordeon__content").style.maxHeight = "0";
+//     }
+//   });
+
+//   if (!isActive) {
+//     item.classList.add("active");
+//     content.style.maxHeight = content.scrollHeight + "px";
+//   } else {
+//     item.classList.remove("active");
+//     content.style.maxHeight = "0";
+//   }
+
+//   if (isMobile) {
+//     let totalContentHeight = 0;
+//     accordeonItems.forEach((otherItem) => {
+//       if (otherItem.classList.contains("active")) {
+//         totalContentHeight += otherItem.querySelector(".accordeon__content").scrollHeight;
+//       }
+//     });
+
+//     const newImageHeight = originalImageHeight - totalContentHeight;
+//     image.style.maxHeight = newImageHeight + "px";
+//   } else {
+//     image.style.maxHeight = "100%";
+//   }
+// }
+
+// accordeonItems.forEach((item, index) => {
+//   const title = item.querySelector(".accordeon__title");
+
+//   title.addEventListener("click", () => {
+//     toggleAccordionItem(item);
+//   });
+
+//   if (index === 0) {
+//     item.classList.add("active");
+//     toggleAccordionItem(item);
+//   }
+// });
+
+// window.addEventListener("resize", () => {
+//   isMobile = window.innerWidth < 725;
+//   if (isMobile) {
+//     autoOpenSecondAccordionItem();
+//   } else {
+//     image.style.maxHeight = "100%";
+//   }
+// });
+// let currentAccordeonItem = 0;
+// const hairBoosterSection = document.getElementById('hair-booster');
+// let shouldScroll = true;
+
+// function openNextAccordeonItem() {
+//   if (currentAccordeonItem < accordeonItems.length) {
+
+//     accordeonItems.forEach(item => {
+//       item.classList.remove('active');
+//       item.querySelector('.accordeon__content').style.maxHeight = '0';
+//     });
+
+
+//     accordeonItems[currentAccordeonItem].classList.add('active');
+//     accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').style.maxHeight =
+//       accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').scrollHeight + 'px';
+
+
+//     currentAccordeonItem++;
+//     docSlider.enable(false);
+//   } else {
+//     shouldScroll = true;
+
+//     docSlider.enable(true);
+//   }
+
+// }
+
+// hairBoosterSection.addEventListener('wheel', (event) => {
+//   if (!shouldScroll) return;
+
+//   const direction = event.deltaY > 0 ? 1 : -1;
+
+//   if (direction === 1) {
+//     openNextAccordeonItem();
+
+//   } else if (direction === -1 && currentAccordeonItem > 0) {
+//     currentAccordeonItem--;
+
+//     accordeonItems.forEach(item => {
+//       item.classList.remove('active');
+//       item.querySelector('.accordeon__content').style.maxHeight = '0';
+
+//     });
+
+//     accordeonItems[currentAccordeonItem].classList.add('active');
+//     accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').style.maxHeight =
+//       accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').scrollHeight + 'px';
+//   }
+
+//   event.preventDefault();
+// });
+// function autoOpenSecondAccordionItem() {
+//   if (window.innerWidth < 725) {
+
+//     if (accordeonItems.length > 1) {
+
+//       accordeonItems.forEach((item) => {
+//         item.classList.remove("active");
+//         item.querySelector(".accordeon__content").style.maxHeight = "0";
+//       });
+
+
+//       const secondItem = accordeonItems[1];
+//       secondItem.classList.add("active");
+//       const content = secondItem.querySelector(".accordeon__content");
+//       content.style.maxHeight = content.scrollHeight + "px";
+//       if (isMobile) {
+//         let totalContentHeight = 0;
+//         accordeonItems.forEach((otherItem) => {
+//           if (otherItem.classList.contains("active")) {
+//             totalContentHeight += otherItem.querySelector(".accordeon__content").scrollHeight;
+//           }
+//         });
+    
+//         const newImageHeight = originalImageHeight - totalContentHeight;
+//         image.style.maxHeight = newImageHeight + "px";
+//       } else {
+//         image.style.maxHeight = "100%";
+//       }
+   
+//     }
+//   } else {
+
+//     if (accordeonItems.length > 0) {
+
+//       accordeonItems.forEach((item) => {
+//         item.classList.remove("active");
+//         item.querySelector(".accordeon__content").style.maxHeight = "0";
+//       });
+
+
+//       const firstItem = accordeonItems[0];
+//       firstItem.classList.add("active");
+//       const content = firstItem.querySelector(".accordeon__content");
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   }
+// }
+
+
+// window.addEventListener("load", autoOpenSecondAccordionItem);
+// window.addEventListener("resize", autoOpenSecondAccordionItem);
+
+
 const accordeonItems = document.querySelectorAll(".accordeon__item");
 const image = document.querySelector(".hair-product--img");
 
@@ -27,7 +193,9 @@ function toggleAccordionItem(item) {
     let totalContentHeight = 0;
     accordeonItems.forEach((otherItem) => {
       if (otherItem.classList.contains("active")) {
-        totalContentHeight += otherItem.querySelector(".accordeon__content").scrollHeight;
+        totalContentHeight += otherItem.querySelector(
+          ".accordeon__content"
+        ).scrollHeight;
       }
     });
 
@@ -60,22 +228,23 @@ window.addEventListener("resize", () => {
   }
 });
 let currentAccordeonItem = 0;
-const hairBoosterSection = document.getElementById('hair-booster');
+
+const hairBoosterSection = document.getElementById("hair-booster");
 let shouldScroll = true;
 
 function openNextAccordeonItem() {
   if (currentAccordeonItem < accordeonItems.length) {
-
-    accordeonItems.forEach(item => {
-      item.classList.remove('active');
-      item.querySelector('.accordeon__content').style.maxHeight = '0';
+    accordeonItems.forEach((item) => {
+      item.classList.remove("active");
+      item.querySelector(".accordeon__content").style.maxHeight = "0";
     });
 
-
-    accordeonItems[currentAccordeonItem].classList.add('active');
-    accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').style.maxHeight =
-      accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').scrollHeight + 'px';
-
+    accordeonItems[currentAccordeonItem].classList.add("active");
+    accordeonItems[currentAccordeonItem].querySelector(
+      ".accordeon__content"
+    ).style.maxHeight =
+      accordeonItems[currentAccordeonItem].querySelector(".accordeon__content")
+        .scrollHeight + "px";
 
     currentAccordeonItem++;
     docSlider.enable(false);
@@ -84,43 +253,54 @@ function openNextAccordeonItem() {
 
     docSlider.enable(true);
   }
-
 }
+let isHairBoosterSectionHovered = false;
 
-hairBoosterSection.addEventListener('wheel', (event) => {
-  if (!shouldScroll) return;
+// Сделал открытие аккордеона только при наведении на него, но сейчас проблема в наложении скролов
 
-  const direction = event.deltaY > 0 ? 1 : -1;
-
-  if (direction === 1) {
-    openNextAccordeonItem();
-
-  } else if (direction === -1 && currentAccordeonItem > 0) {
-    currentAccordeonItem--;
-
-    accordeonItems.forEach(item => {
-      item.classList.remove('active');
-      item.querySelector('.accordeon__content').style.maxHeight = '0';
-
-    });
-
-    accordeonItems[currentAccordeonItem].classList.add('active');
-    accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').style.maxHeight =
-      accordeonItems[currentAccordeonItem].querySelector('.accordeon__content').scrollHeight + 'px';
-  }
-
-  event.preventDefault();
+hairBoosterSection.addEventListener("mouseenter", function () {
+  isHairBoosterSectionHovered = true;
 });
-function autoOpenSecondAccordionItem() {
-  if (window.innerWidth < 725) {
 
-    if (accordeonItems.length > 1) {
+hairBoosterSection.addEventListener("mouseleave", function () {
+  isHairBoosterSectionHovered = false;
+});
+
+document.addEventListener("wheel", function (event) {
+  if (isHairBoosterSectionHovered) {
+    if (!shouldScroll) return;
+
+    const direction = event.deltaY > 0 ? 1 : -1;
+
+    if (direction === 1) {
+      openNextAccordeonItem();
+    } else if (direction === -1 && currentAccordeonItem > 0) {
+      currentAccordeonItem--;
 
       accordeonItems.forEach((item) => {
         item.classList.remove("active");
         item.querySelector(".accordeon__content").style.maxHeight = "0";
       });
 
+      accordeonItems[currentAccordeonItem].classList.add("active");
+      accordeonItems[currentAccordeonItem].querySelector(
+        ".accordeon__content"
+      ).style.maxHeight =
+        accordeonItems[currentAccordeonItem].querySelector(
+          ".accordeon__content"
+        ).scrollHeight + "px";
+    }
+
+    event.preventDefault();
+  }
+});
+function autoOpenSecondAccordionItem() {
+  if (window.innerWidth < 725) {
+    if (accordeonItems.length > 1) {
+      accordeonItems.forEach((item) => {
+        item.classList.remove("active");
+        item.querySelector(".accordeon__content").style.maxHeight = "0";
+      });
 
       const secondItem = accordeonItems[1];
       secondItem.classList.add("active");
@@ -130,26 +310,24 @@ function autoOpenSecondAccordionItem() {
         let totalContentHeight = 0;
         accordeonItems.forEach((otherItem) => {
           if (otherItem.classList.contains("active")) {
-            totalContentHeight += otherItem.querySelector(".accordeon__content").scrollHeight;
+            totalContentHeight += otherItem.querySelector(
+              ".accordeon__content"
+            ).scrollHeight;
           }
         });
-    
+
         const newImageHeight = originalImageHeight - totalContentHeight;
         image.style.maxHeight = newImageHeight + "px";
       } else {
         image.style.maxHeight = "100%";
       }
-   
     }
   } else {
-
     if (accordeonItems.length > 0) {
-
       accordeonItems.forEach((item) => {
         item.classList.remove("active");
         item.querySelector(".accordeon__content").style.maxHeight = "0";
       });
-
 
       const firstItem = accordeonItems[0];
       firstItem.classList.add("active");
@@ -159,8 +337,5 @@ function autoOpenSecondAccordionItem() {
   }
 }
 
-
 window.addEventListener("load", autoOpenSecondAccordionItem);
 window.addEventListener("resize", autoOpenSecondAccordionItem);
-
-
