361adfb9cda93b1e3d996cefd9e8f5027d5ebf3f
Georgiy Sichinava <danzels5000@gmail.com>
2023-11-09 22:30:21 +0300

    fix responsive

--- js/multipage.js
+++ js/multipage.js
@@ -1,4 +1,4 @@
-if (window.innerWidth <= 1024 && window.innerHeight > 375) {
+if (window.innerWidth <= 1024 && window.innerHeight > 325) {
   const pages = document.querySelectorAll(".body-product__item");
   const productsContainer = document.getElementById("body-products");
   let currentPage = 0;
