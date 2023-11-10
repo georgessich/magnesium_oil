b2f923a51c7d6e8bd7d21d215b5443615595a704
Georgiy Sichinava <danzels5000@gmail.com>
2023-11-10 09:29:04 +0300

    new trackpad code

--- js/trackpad.js
+++ js/trackpad.js
@@ -1,26 +1,24 @@
 let isScrolling = false;
 
-document.addEventListener('wheel', function (event) {
-    if (isScrolling) return;
-    isScrolling = true;
-    setTimeout(function () {
-        isScrolling = false;
-    }, 100);
-
-    if (event.deltaMode !== 0) {
-        event.preventDefault();
-        const maxScrollDelta = 200;
-        const deltaY = event.deltaY > 0 ? Math.min(event.deltaY, maxScrollDelta) : Math.max(event.deltaY, -maxScrollDelta);
-        const direction = deltaY > 0 ? 'next' : 'prev';
-        const to = direction === 'next' ? docSlider.getCurrentIndex() + 1 : docSlider.getCurrentIndex() - 1;
-        if ((direction === 'next' && to >= docSlider.getElements().pages.length) ||
-            (direction === 'prev' && to < 0)) {
-            return;
-        }
-        if (direction === 'next') {
-            docSlider.nextPage();
-        } else {
-            docSlider.prevPage();
-        }
+function detectTrackPad(e) {
+  if (!isScrolling) {
+    let isTrackpad = false;
+    if (e.wheelDeltaY) {
+      if (e.wheelDeltaY === (e.deltaY * -3)) {
+        isTrackpad = true;
+      }
+    } else if (e.deltaMode === 0) {
+      isTrackpad = true;
     }
-});
+    if (isTrackpad) {
+      isScrolling = true; 
+      console.log("Трекпад работает");
+      setTimeout(function () {
+        isScrolling = false;
+      }, 1000); 
+    } else {
+      console.log("Колесо мышки работает");
+    }
+  }
+}
+document.addEventListener("wheel", detectTrackPad, false);
